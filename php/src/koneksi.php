c03ba9b64e31d2fb664e25a5f24b5ff69b39e33b
qorina12 <qorinaprameswari12@gmail.com>
2021-12-23 09:33:32 +0700

    Tugas CICD

---
+++ php/src/koneksi.php
@@ -0,0 +1,11 @@
+<?php 
+    $server = "172.20.0.2"; 
+    $user = "orin"; 
+    $password = "Admin"; 
+    $nama_database = "dbpus"; 
+
+    $db = mysqli_connect($server, $user, $password, $nama_database); 
+    if( !$db ) {
+        die("Gagal terhubung dengan database: " . mysqli_connect_error()); 
+    }
+?>
